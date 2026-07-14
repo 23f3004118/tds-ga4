d16f65dd98e604f0df544b7afcbb503a9c196bc9
23f3004118 <23f3004118@ds.study.iitm.ac.in>
2026-07-14 12:16:36 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-EMAIL = "23f3004118@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjMwMDQxMThAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MzY5NzgzNCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDMwMjYzNH0.ZCuh4RXt576HEUTGf6YIHOHvNsvIJGo3fr8JZzBDQFM"
-AIPIPE_BASE = "https://aipipe.org/openai/v1"
-TEXT_MODEL = "gpt-4o"
